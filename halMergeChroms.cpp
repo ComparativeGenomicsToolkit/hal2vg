af1fd0a2a7e62c19a94cba03839082f560458766
Glenn Hickey <glenn.hickey@gmail.com>
2021-03-29 19:37:57 -0400

    get halMergeChroms running

--- halMergeChroms.cpp
+++ halMergeChroms.cpp
@@ -29,33 +29,62 @@ static void initParser(CLParser* optionsParser) {
         ". Star trees only");
 }
 
-// get the dimensions from a file
-static pair<string, unordered_map<string, vector<Sequence::Info>>> get_hal_dimensions(CLParser* optionsParser, const string& hal_path) {
+// we expect to see the same ancestor sequence names in multiple input files. we uniqify them by adding
+// .i to them where i is the file's position in the input.
+static string anc_seq_name(const string& seq_name, size_t idx) {
+    return seq_name + ".hmc" + to_string(idx);
+}
+// undo the above
+static string orig_seq_name(const string& seq_name) {
+    return seq_name.substr(0, seq_name.rfind(".hmc"));
+}
 
-    // open the hal file
-    AlignmentConstPtr alignment(openHalAlignment(hal_path, optionsParser, READ_ACCESS));
-
-    // our output map
+// get the dimensions from all genomes in all input files
+static pair<string, unordered_map<string, vector<Sequence::Info>>> get_hal_dimensions(CLParser* optionsParser,
+                                                                                      const vector<string>& hal_paths) {
+    // genome -> dimensions (covering all input)
     unordered_map<string, vector<Sequence::Info>> dimensions;
-    
-    // for every genome
-    vector<string> genome_names = alignment->getChildNames(alignment->getRootName());
-    genome_names.push_back(alignment->getRootName());
-    for (const string& genome_name : genome_names) {
-        const Genome* genome = alignment->openGenome(genome_name);
-        vector<Sequence::Info>& genome_dimensions = dimensions[genome_name];
-        // for every sequence
-        for (SequenceIteratorPtr seqIt = genome->getSequenceIterator(); not seqIt->atEnd(); seqIt->toNext()) {
-            const Sequence *sequence = seqIt->getSequence();
-            genome_dimensions.emplace_back(sequence->getName(),
-                                           sequence->getSequenceLength(),
-                                           sequence->getNumTopSegments(),
-                                           sequence->getNumBottomSegments());
-        }
-        alignment->closeGenome(genome);        
-    }
+    // to check uniqueness
+    unordered_set<string> sequence_names;
 
-    return make_pair(alignment->getRootName(), dimensions);
+    string root_name;
+    for (size_t i = 0; i < hal_paths.size(); ++i) {
+        const string& hal_path = hal_paths[i];
+        
+        // open the hal file
+        AlignmentConstPtr alignment(openHalAlignment(hal_path, optionsParser, READ_ACCESS));
+    
+        // for every genome
+        vector<string> genome_names = alignment->getChildNames(alignment->getRootName());
+        genome_names.push_back(alignment->getRootName());
+        if (root_name.empty()) {
+            root_name = alignment->getRootName();
+        } else if (alignment->getRootName() != root_name) {
+            throw hal_exception("Root mismatch: " + root_name + " vs " + alignment->getRootName());        
+        }
+        for (const string& genome_name : genome_names) {
+            const Genome* genome = alignment->openGenome(genome_name);
+            vector<Sequence::Info>& genome_dimensions = dimensions[genome_name];
+            // for every sequence
+            for (SequenceIteratorPtr seqIt = genome->getSequenceIterator(); not seqIt->atEnd(); seqIt->toNext()) {
+                const Sequence *sequence = seqIt->getSequence();
+                // add a little suffix to make ancestral sequences unique
+                string seq_name = genome->getParent() ? sequence->getName() : anc_seq_name(sequence->getName(), i);
+                genome_dimensions.emplace_back(seq_name,
+                                               sequence->getSequenceLength(),
+                                               sequence->getNumTopSegments(),
+                                               sequence->getNumBottomSegments());
+                string full_name = genome_name + "." + seq_name;
+                if (sequence_names.count(full_name)) {
+                    throw hal_exception("Duplicate sequence name found: " + full_name);
+                } else {
+                    sequence_names.insert(full_name);
+                }
+            }
+            alignment->closeGenome(genome);        
+        }
+    }
+    return make_pair(root_name, dimensions);
 }
 
 // append each input hal to the out_alignment, one after another.  all arrays are copied over,
@@ -73,6 +102,26 @@ static void merge_hals(CLParser* optionsParser, AlignmentPtr out_alignment, cons
         assert(in_root->getName() == out_root->getName());
         size_t in_root_degree = in_root->getNumChildren();
         vector<const Genome*> in_genomes = {in_root};
+        for (const string& in_child_name : in_alignment->getChildNames(in_root->getName())) {
+            in_genomes.push_back(in_alignment->openGenome(in_child_name));
+        }
+
+        // copy the dna sequence by sequence
+        for (const Genome* in_genome : in_genomes) {
+            cerr << "[halMergeChroms]: copying dna for " << in_genome->getName() << " from " << in_paths[i] << endl;
+            Genome* out_genome = out_alignment->openGenome(in_genome->getName());
+            for (SequenceIteratorPtr in_si = in_genome->getSequenceIterator(); !in_si->atEnd(); in_si->toNext()) {
+                const Sequence* in_sequence = in_si->getSequence();
+                string out_seq_name = in_genome->getParent() ? in_sequence->getName() : anc_seq_name(in_sequence->getName(), i);
+                Sequence* out_sequence = out_genome->getSequence(out_seq_name);
+                DnaIteratorPtr in_di = in_sequence->getDnaIterator(0);
+                DnaIteratorPtr out_di = out_sequence->getDnaIterator(0);
+                assert(in_sequence->getSequenceLength() == out_sequence->getSequenceLength());
+                string dna;
+                in_sequence->getString(dna);
+                out_sequence->setString(dna);
+            }
+        }
 
         // make a child index map (in -> out) for the root genome
         // assume : all genomes in in_genome present in out_genome
@@ -83,30 +132,40 @@ static void merge_hals(CLParser* optionsParser, AlignmentPtr out_alignment, cons
         }
 
         // copy over the bottom segments of the root
+        cerr << "[halMergeChroms]: copying bottom segments for " << in_root->getName() << " from " << in_paths[i]
+             << " with bseg offset " << bot_offset << endl;
         BottomSegmentIteratorPtr in_bi = in_root->getBottomSegmentIterator(0);
         BottomSegmentIteratorPtr out_bi = out_root->getBottomSegmentIterator(bot_offset);
         for (;!in_bi->atEnd(); in_bi->toRight(), out_bi->toRight()) {
             // set the segment in the root genome
             assert(out_bi->bseg()->getArrayIndex() == in_bi->bseg()->getArrayIndex() + bot_offset);
+            assert(out_bi->bseg()->getNumChildren() >= in_bi->bseg()->getNumChildren());
             out_bi->bseg()->setTopParseIndex(NULL_INDEX);
             // determine the sequence-relative coordinate in the input
             const Sequence* in_sequence = in_bi->bseg()->getSequence();
             int64_t in_start_coord = in_bi->bseg()->getStartPosition() - in_sequence->getStartPosition();
+            assert(in_start_coord >= 0 && in_start_coord < in_sequence->getSequenceLength());
             // set the sequence relative coordinate in the output
-            const Sequence* out_sequence = out_root->getSequence(in_sequence->getName());
+            const Sequence* out_sequence = out_root->getSequence(anc_seq_name(in_sequence->getName(), i));
             int64_t out_start_coord = out_sequence->getStartPosition() + in_start_coord;
+            assert(out_start_coord >= 0 && out_start_coord < out_root->getSequenceLength());
             out_bi->bseg()->setCoordinates(out_start_coord, in_bi->bseg()->getLength());
-
             // set the segment in the child genomes
             for (size_t in_ci = 0; in_ci < in_root_degree; ++in_ci) {
-                size_t out_ci = in_ci_to_out_ci[in_ci];
-                out_bi->bseg()->setChildIndex(out_ci, in_bi->bseg()->getChildIndex(in_ci) + top_offsets[out_ci]);
-                out_bi->bseg()->setChildReversed(out_ci, in_bi->bseg()->getChildReversed(in_ci));
+                size_t out_ci = in_ci_to_out_ci.at(in_ci);
+                assert(out_ci < out_bi->bseg()->getNumChildren());
+                if (in_bi->bseg()->hasChild(in_ci)) {
+                    out_bi->bseg()->setChildIndex(out_ci,  in_bi->bseg()->getChildIndex(in_ci) + top_offsets[out_ci]);
+                    out_bi->bseg()->setChildReversed(out_ci, in_bi->bseg()->getChildReversed(in_ci));
+                } else {
+                    out_bi->bseg()->setChildIndex(out_ci,  NULL_INDEX);
+                }
             }
         }
 
         // for every child genome, copy over the top segments
         for (const string& in_child_name : in_alignment->getChildNames(in_root->getName())) {
+            cerr << "[halMergeChroms]: copying top segments for " << in_child_name << " from " << in_paths[i] << endl;
             const Genome* in_child = in_alignment->openGenome(in_child_name);
             Genome* out_child = out_alignment->openGenome(in_child_name);
             
@@ -125,7 +184,7 @@ static void merge_hals(CLParser* optionsParser, AlignmentPtr out_alignment, cons
                 const Sequence* in_sequence = in_ti->tseg()->getSequence();
                 int64_t in_start_coord = in_ti->tseg()->getStartPosition() - in_sequence->getStartPosition();
                 // set the sequence relative coordinate in the output
-                const Sequence* out_sequence = out_root->getSequence(in_sequence->getName());
+                const Sequence* out_sequence = out_child->getSequence(in_sequence->getName());
                 int64_t out_start_coord = out_sequence->getStartPosition() + in_start_coord;
                 out_ti->tseg()->setCoordinates(out_start_coord, in_ti->tseg()->getLength());
                 // set the paralogy edge
@@ -134,19 +193,6 @@ static void merge_hals(CLParser* optionsParser, AlignmentPtr out_alignment, cons
                 } else {
                     out_ti->tseg()->setNextParalogyIndex(NULL_INDEX);
                 }
-            }
-            in_genomes.push_back(in_child);
-        }
-
-        // copy the dna sequence by sequence
-        for (const Genome* in_genome : in_genomes) {
-            Genome* out_genome = out_alignment->openGenome(in_genome->getName());
-            for (SequenceIteratorPtr in_si = in_genome->getSequenceIterator(); !in_si->atEnd(); in_si->toNext()) {
-                const Sequence* in_sequence = in_si->getSequence();
-                Sequence* out_sequence = out_genome->getSequence(in_sequence->getName());
-                string dna;
-                in_sequence->getString(dna);
-                out_sequence->setString(dna);
             }
         }
 
@@ -180,48 +226,23 @@ int main(int argc, char** argv) {
     vector<string> in_paths = chopString(in_hal_paths, ",");
     
     // map genome -> dimensions for each input alignment
-    vector<unordered_map<string, vector<Sequence::Info>>> dimensions_list;
-    string root_name;
-    for (const string& in_path : in_paths) {
-        auto rd = get_hal_dimensions(&optionsParser, in_path);
-        dimensions_list.push_back(rd.second);
-        if (root_name.empty()) {
-            root_name = rd.first;
-            assert(in_path == in_paths[0]);
-        } else if (rd.first != root_name) {
-            throw hal_exception("Root mismatch: " + rd.first + " vs " + root_name);
-        }
-    }
-
-    // check uniqueness and sum up
-    // todo: resolve ancestor name collisions    
-    unordered_set<string> sequence_names;
-    unordered_map<string, vector<Sequence::Info>> total_dimensions;    
-    for (const unordered_map<string, vector<Sequence::Info>>& dimensions : dimensions_list) {
-        for (const auto& kv : dimensions) {
-            for (const Sequence::Info& sequence_info : kv.second) {
-                string name = kv.first + "." + sequence_info._name;
-                if (sequence_names.count(name)) {
-                    throw hal_exception("Conflict: sequence name found in more than one file: " + name);
-                }
-                sequence_names.insert(name);
-            }
-            total_dimensions[kv.first].insert(total_dimensions[kv.first].begin(), kv.second.begin(), kv.second.end());
-        }
-    }
+    pair<string, unordered_map<string, vector<Sequence::Info>>> rd = get_hal_dimensions(&optionsParser, in_paths);
+    string& root_name = rd.first;
+    unordered_map<string, vector<Sequence::Info>>& dimensions = rd.second;
 
     // create the new file
     AlignmentPtr alignment(openHalAlignment(out_hal_path, &optionsParser, READ_ACCESS | WRITE_ACCESS | CREATE_ACCESS));
 
-    // set up the size of each genome
+    // set up the size of each genome, staring with the root
     Genome* root_genome = alignment->addRootGenome(root_name);
-    root_genome->setDimensions(total_dimensions.at(root_name));    
-    for (const auto& gd : total_dimensions) {
-        if (gd.first != root_name) {
-            Genome* leaf_genome = alignment->addLeafGenome(gd.first, root_name, 1);
-            leaf_genome->setDimensions(gd.second);
+    for (auto& kv : dimensions) {
+        if (kv.first != root_name) {
+            Genome* leaf_genome = alignment->addLeafGenome(kv.first, root_name, 1);
+            leaf_genome->setDimensions(kv.second);
         }
     }
+    // important to set root dimensions after adding leaves so bottom segments have right number of slots
+    root_genome->setDimensions(dimensions.at(root_name));
 
     // copy over over everything
     merge_hals(&optionsParser, alignment, in_paths);
